c92f0c15a42968ab3c6da6df9693d6e553191208
Daniel Calauad Ramos <1717198@cclab.usls.edu>
2026-09-15 13:31:27 +0800

    Test commit: adding the raw data needed

---
+++ Exam.py
@@ -0,0 +1,10 @@
+
+
+raw_rows = [
+    {"name": " Amara ", "scores": "92,85,78"},
+    {"name": "Leo", "scores": "88,91,73"},
+    {"name": "Priya", "scores": "65,72,150"},         
+    {"name": "Sam", "scores": "70,not_a_number,60"},  
+    {"name": "Amara", "scores": "95,90,88"},          
+    {"name": "Jade", "scores": "81,77,84,90"},
+]

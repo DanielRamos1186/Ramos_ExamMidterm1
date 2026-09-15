f8bd1ae00bd48d3470ce144025fe6828fc44eeed
Daniel Calauad Ramos <1717198@cclab.usls.edu>
2026-09-15 14:22:53 +0800

    semi truck

--- Exam.py
+++ Exam.py
@@ -1,25 +1,6 @@
 import pandas as pd
 import numpy as np
 
-#malformed score data, duplicates 
-
-#the code must clean, refuse the data that it cannot and never crash due to a bad single row
-
-#student_record = (self, name, score) [NumPy array] || if record is built = .lock() || .add_score() method that adheres to the .lock()
-
-#if .lock() = true, then .add_score() is false
-
-#.average() method using np.mean() that gets the average per student once .lock() is true 
-
-#class InvalidScoreError(Exception): whenever the score in the row of the student[name] is invalid || __str__ "that score is not a number" or "the score is not within 0 - 100" || __repr__ same error but for debugging 
-
-#class StudentRecordLockedError(Exception): whenever a value is added to a .lock() student record || __str__ "The student record is locked and cannot be added any new scores"
-
-#function that cleans the raw_rows with pandas trimming, parsing, and dropping of duplicates. It then builds the cleaned rows 
-
-#exception handling that tracks whenever the rows get them for (student, failures) format: {"row": <original raw dict>, "error": <reason>} duplicates are not part of the failure list but must be cleaned 
-
-#function ranking uses sorted() with lambda key to sort average() 
 
 #raw data to be cleaned 
 raw_rows = [
@@ -33,9 +14,26 @@ raw_rows = [
 
 class Student: 
     def __init__ (self, name, score):
-        
+        self.name = name
+        self.score = score
+
+    def lock():
+        if Record = built
+            Record = true 
+        else 
+            print(f"record is not lock")
+            
+    def add_score():
 
 
-class InsufficientFundsError(Exception):
 
+class InvalidScoreError(Exception):
+    def __init__(self, name, score)
+        message=f"that score is not a number {score}"
+
+    def __str__(self)
+        return f"{self.name}" 
+    
+    def __repr__(self)
+        return f"{self,name}" 
 class StudentRecordLockedError(Exception):
